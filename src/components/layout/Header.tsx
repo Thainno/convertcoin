3b9ee0c0a5ce662f4cc74888cbca4696b3039f84
Tainu <thainnosv@gmail.com>
2025-03-20 17:19:23 -0300

    header upgrade

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,16 +1,30 @@
+import Image from "next/image";
+import bgHeader from "@/assets/images/bg-header.svg";
+import logo from "@/assets/images/logo.svg";
 import "@/styles/components/layout/header.css";
 
 export default function Header() {
   return (
-    <header className="test">
-      <img></img>
-      <nav>
-        <ul>
-          <li>Apoie o projeto</li>
-          <li>Sobre nós</li>
-        </ul>
-      </nav>
-      <img></img>
+    <header className="header">
+      {/*background image*/}
+      <Image
+        className="bg-header"
+        src={bgHeader}
+        alt="Imagem de fundo do cabeçalho"
+      />
+      <div className="header-content">
+        <Image src={logo} width={150} alt="Logo Convert Coin" />
+        <nav className="nav-header">
+          <ul className="flex flex-row gap-25">
+            <li>
+              <a className="cursor-pointer">Sobre nós</a>
+            </li>
+            <li>
+              <a className="cursor-pointer">Apoie o projeto</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 }
